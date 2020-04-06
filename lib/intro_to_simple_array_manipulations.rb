16c7f1f6d467e7a15be5229eceb42aee50f86799
Philip Roush <roush.philip@gmail.com>
2020-04-06 10:59:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,7 @@ def using_flatten(instruments)
 end
 
 def using_delete(instructors, no_offense_steven)
-  no_offense_steven.delete(instructors, "Steven")
+  no_offense_steven = instructors.delete("Steven")
 end
 
 def using_delete_at(famous_robots, deleted_robot)
